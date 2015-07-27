634f306b54bd09a03ac87e0bcb75e28bf3bf49d7
Steve Witman <stevewitman@gmail.com>
2015-07-26 23:50:52 -0600

    created signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create
+    user  = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Signed In"
+    else
+      flash.now.alert = "Email or password invalid"
+      render "new"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
   resources :users
+  resources :sessions
 
   root 'home#index'
 
