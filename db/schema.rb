2361a8d111383d1cb2fe0cad2b4ff622cb2502d2
Steve Witman <stevewitman@gmail.com>
2015-07-27 15:19:03 -0600

    added remember_me and changed to auth token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727014315) do
+ActiveRecord::Schema.define(version: 20150727145839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150727014315) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "auth_token"
   end
 
 end
