9694f3cea2a782b6ad3697159c0569f6f0f79925
Steve Witman <stevewitman@gmail.com>
2015-08-18 23:07:50 -0600

    start link index page

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,9 +4,21 @@ class LinksController < ApplicationController
   def index
     @links = Link.all
     @link = Link.new
-
   end
 
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to links_path, notice: 'Link was added'
+    else
+      render :new
+    end
+  end
 
+private
+
+  def link_params
+    params.require(:link).permit(:title, :url, :linkr_url, :description, :status, :rating)
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,5 @@ private
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
+
 end
